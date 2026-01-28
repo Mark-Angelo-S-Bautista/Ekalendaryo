1f51d53bb6f3214bcad2eaae2432d7593f9ff0f0
Mark Angelo Bautista <179707263+Mark-Angelo-S-Bautista@users.noreply.github.com>
2026-01-28 12:39:16 +0800

    pagination changed

--- resources/views/vendor/pagination/simple.blade.php
+++ resources/views/vendor/pagination/simple.blade.php
@@ -7,22 +7,10 @@
             <a href="{{ $paginator->previousPageUrl() }}">Prev</a>
         @endif
 
-        {{-- Pages --}}
-        @foreach ($elements as $element)
-            @if (is_string($element))
-                <span class="dots">{{ $element }}</span>
-            @endif
-
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <span class="active">{{ $page }}</span>
-                    @else
-                        <a href="{{ $url }}">{{ $page }}</a>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+        {{-- Current / Total --}}
+        <span class="current-page">
+            {{ $paginator->currentPage() }} of {{ $paginator->lastPage() }}
+        </span>
 
         {{-- Next --}}
         @if ($paginator->hasMorePages())
