9d393af7b2f4ede34b0e3b071e268195f1a5fd1d
Mark Angelo Bautista <179707263+Mark-Angelo-S-Bautista@users.noreply.github.com>
2026-02-07 15:24:36 +0800

    Update notifications.blade.php

--- resources/views/Editor/notifications.blade.php
+++ resources/views/Editor/notifications.blade.php
@@ -6,7 +6,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>eKalendaryo - Notifications</title>
-        @vite(['resources/css/viewer/notifications.css'])
+        @vite(['resources/css/editor/notifications.css'])
     </head>
 
     <body>
